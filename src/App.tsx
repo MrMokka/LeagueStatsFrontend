74ac720d8e9d84fdfd7543c79f045f6439ac980c
MrMokka <sartaren@hotmail.com>
2020-09-28 17:13:56 +0200

    trying to cast json to custom type, need work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import {SummonerData, MatchListData, MatchData,
+    ParticipantData, ParticipantIdentitiesData} from './typeList'
+
+let tempDataFile = require('./tempData.json');
+
+const tempSummonerDataa: SummonerData = {
+    accountId: tempDataFile["summoner"]["accountId"],
+    name: tempDataFile["summoner"]["name"]
+};
+
+const temptMatchData = tempDataFile["matchData"] as MatchData;
+const tempData = {
+    // summoner: JSON.parse(tempDataFile["summoner"]) as SummonerData,
+    // matches: JSON.parse(tempDataFile["matches"]) as MatchListData[],
+    // matchData: JSON.parse(tempDataFile["matchData"]) as MatchData
+}
+
 
 function App() {
+    console.log(tempSummonerDataa);
+    console.log(temptMatchData);
     return (
         <div className="App">
             
-            {/*test*/}
+            
             
         </div>
     );
