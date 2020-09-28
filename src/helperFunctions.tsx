7e023a323a0f6ae698d1ec5a2edf8e0e2ffab150
MrMokka <sartaren@hotmail.com>
2020-09-28 17:13:26 +0200

    added type and helper files

---
+++ src/helperFunctions.tsx
@@ -0,0 +1,8 @@
+
+
+export function getChampionById(id: number){
+    return "Shen" //TODO: Make API call to get correct champion
+}
+
+
+
---
+++ src/typeList.tsx
@@ -0,0 +1,35 @@
+
+export type SummonerData = {
+    accountId: number,
+    name: string
+};
+
+export type MatchListData = {
+    gameId: number,
+    champion: number,
+    timestamp: number,
+    role: string,
+    lane: string
+};
+
+export type ParticipantData = {
+
+};
+
+export type ParticipantIdentitiesData = {
+    participantId: number,
+    player: {
+        accountId: string,
+        summonerName: string,
+        summonerId: string
+    }
+};
+
+export type MatchData = {
+    gameId: number,
+    participants: ParticipantData[],
+    participantIdentities: ParticipantIdentitiesData[],
+};
+
+
+
